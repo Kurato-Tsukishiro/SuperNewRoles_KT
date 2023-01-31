644766d95a54229fb38d95d77ffa949db0ef5dcb
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-01-31 15:51:54 +0900

    プライベートな変数の先頭を小文字に直した

--- SuperNewRoles/Mode/TaskPractice/TaskPracticeOption.cs
+++ SuperNewRoles/Mode/TaskPractice/TaskPracticeOption.cs
@@ -2,11 +2,11 @@ namespace SuperNewRoles.Mode.TaskPractice
 {
     class TaskPracticeOption
     {
-        private const int OptionId = 1099;// 設定のId
+        private const int optionId = 1099;// 設定のId
         public static CustomOption TaskPracticeMode;
         public static void Load()
         {
-            TaskPracticeMode = CustomOption.Create(OptionId, true, CustomOptionType.Generic, "SettingTaskPracticeMode", false, ModeHandler.ModeSetting);
+            TaskPracticeMode = CustomOption.Create(optionId, true, CustomOptionType.Generic, "SettingTaskPracticeMode", false, ModeHandler.ModeSetting);
         }
     }
 }
